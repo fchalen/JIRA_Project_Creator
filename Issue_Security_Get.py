9ca0685ab0dd14cc38591d7a11fa6c8e94b0ad48
fchalen <63106273+fchalen@users.noreply.github.com>
2022-02-17 10:38:09 -0300

    Update Issue_Security_Get.py

--- Issue_Security_Get.py
+++ Issue_Security_Get.py
@@ -2,16 +2,21 @@ import requests
 from requests.auth import HTTPBasicAuth
 import json
 
-#Issue_Sec_Scheme = "issuesecurityschemes"
-#Permission_Scheme = "permissionscheme"
-#Notif_Scheme = "notificationscheme"
-#P_Category = "projectCategory"
-#Wflow_Scheme = "workflowscheme"
-
+'''
+change the url variable ending to retrieve different data
+Issue_Sec_Scheme = "api/2/issue/issuesecurityschemes"
+Permission_Scheme = "permissionscheme"
+Notif_Scheme = "notificationscheme"
+P_Category = "api/2/project/projectCategory"
+Wflow_Scheme = "workflowscheme"
+Issue Create Metadata = "api/2/issue/createmeta"
+Project info = "api/2/project/search"
+for more info look for GETs in https://developer.atlassian.com/cloud/jira/platform/rest/v2/api-group-projects/#api-rest-api-2-project-post
+'''
 
 url = "https://iqualldev.atlassian.net/rest/api/2/issue/createmeta"
 
-auth = HTTPBasicAuth("francisco.chalen@iquall.net", "39GnxVeP4l3kuWRBDq8463C3")
+auth = HTTPBasicAuth("francisco.chalen@iquall.net", "TOKEN")
 
 headers = {
    "Accept": "application/json"
